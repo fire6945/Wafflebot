9ed4f63670d446da55eeef849669c63f934548cf
fire6945 <josephdlee08@gmail.com>
2022-09-17 21:02:55 -0400

    Created user info command

--- Wafflebot/bot/bot.py
+++ Wafflebot/bot/bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-from .cogs import events
+from .cogs import events, utility
 from .utility import values
 
 class Wafflebot:
@@ -13,7 +13,7 @@ class Wafflebot:
     def __init__(self, token):
         self.token = token
         self.client = commands.Bot(command_prefix=values.Values.get_prefix(), case_insensitive=True, intents=discord.Intents.all())
-        self.cogs = [events]
+        self.cogs = [events, utility]
 
     def run(self):
         for cog in self.cogs:
